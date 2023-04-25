377afcaa10e0bb911beae966e6494e2d63114bde
Godspower Udokpoh <111070276+Geepeey@users.noreply.github.com>
2023-04-25 22:10:22 +0100

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,17 +6,19 @@ const movieId = process.argv[2];
 const baseUrl = 'https://swapi.dev/api/films/';
 const url = `${baseUrl}${movieId}/`;
 
-request(url, function (error, response, body) {
+request(url, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else {
+  }
+  
     const movie = JSON.parse(body);
     const characters = movie.characters;
-    characters.forEach(function (characterUrl) {
-      request(characterUrl, function (error, response, body) {
+    characters.forEach(characterUrl) {
+      request(characterUrl, (error, response, body) => {
         if (error) {
           console.error(error);
-        } else {
+        }
+        
           const character = JSON.parse(body);
           console.log(character.name);
         }
