b05a9698f8464400bcbfdf31dad17f4ebebb4cda
Godspower Udokpoh <harshmallins@gmail.com>
2023-04-25 04:20:29 -0700

    Count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+
+    for (const film of films) {
+      const characters = film.characters;
+      if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        count++;
+      }
+    }
+
+    console.log(count);
+  }
+});
