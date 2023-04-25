68a39ec00accee3291a896a8c6e835fabfff901d
Godspower Udokpoh <harshmallins@gmail.com>
2023-04-25 04:37:27 -0700

    SW characters

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  
+  const movieData = JSON.parse(body);
+  const characterUrls = movieData.characters;
+  
+  characterUrls.forEach((url) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
+});
