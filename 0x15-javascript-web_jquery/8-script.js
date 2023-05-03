df163ea80e3d2467b91e47bf84f952c6073df15a
Godspower Udokpoh <harshmallins@gmail.com>
2023-05-03 13:59:11 -0700

    Javascript

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,4 +1,4 @@
-$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
   const movies = data.results;
   const $list = $('#list_movies');
 
